3fb44ecfe39ff187820a6708ac2b2e3168f368d9
Steven Ploog <emailstevenp@gmail.com>
2017-01-07 18:33:34 -0500

    Send display/not display lists to server in post. Log at server.
    Also switch all single quotes to double quotes

--- Web/index.js
+++ Web/index.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var vis = require('./visualizer.js');
+var express = require("express");
+var bodyParser = require("body-parser");
+var vis = require("./visualizer.js");
 
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+//app.use(app.router());
+//var urlencodedParser = bodyParser.urlencoded({extended: false});
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// Load the list of visualizers
 vis.loadVisualizers();
 
-app.use(express.static('public'));
+app.post("/", function(req, res) {
+    console.log(req.body);
+    //console.log(req.body.visibleIds);
+    //console.log(req.body.hiddenIds);
+});
 
-app.get('/index.html', function(req, res) {
-    console.log('Got a GET request for /index.html');
+app.get("/index.html", function(req, res) {
+    console.log("Got a GET request for /index.html");
     res.send(vis.getVisualizerSelectHTML());
     //res.sendFile(__dirname + "/" + "index.html");
 });
@@ -19,5 +27,5 @@ var server = app.listen(8080, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Server listening at addr: %s\tport: %s', host, port);
+    console.log("Server listening at addr: %s\tport: %s", host, port);
 });
--- Web/visualizer.js
+++ Web/visualizer.js
@@ -1,9 +1,9 @@
-var readfiles = require('node-readfiles');
-const pug = require('pug');
-const compiledFunction = pug.compileFile('home.pug');
+var readfiles = require("node-readfiles");
+const pug = require("pug");
+const compiledFunction = pug.compileFile("home.pug");
 
 var visualizerData = [];
-var visualizerSelectHTML = '';
+var visualizerSelectHTML = "";
 
 // Load config files and render visualizer selection webpage
 exports.loadVisualizers = function () {
@@ -18,20 +18,20 @@ exports.getVisualizerSelectHTML = function() {
 
 // Read all visualizer config files and parse their contents
 function getConfigFiles() {
-    readfiles(__dirname + '/visualizers/', {
-        filter: '**config.json', // Filter for config files
+    readfiles(__dirname + "/visualizers/", {
+        filter: "**config.json", // Filter for config files
         }, function (err, filename, content) {
             // Error checking
             if (err) {throw err;}
 
             // Parse and add add to visualizerData array
             var parsed = JSON.parse(content);
-            console.log('Loaded config: ' + JSON.stringify(parsed));
+            console.log("Loaded config: " + JSON.stringify(parsed));
             visualizerData.push(parsed);
 
             // Render page
-            visualizerSelectHTML = compiledFunction({'visualizers': visualizerData});
-            console.log('Rendered visualizer select page');
+            visualizerSelectHTML = compiledFunction({"visualizers": visualizerData});
+            console.log("Rendered visualizer select page");
         }
     );
 }
